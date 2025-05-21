77f670afdff2bf813495c46077e44991646288f3
DilukaB <navanjana721@gmail.com>
2025-05-21 15:00:54 +0530

    equipment

--- BOI Training Institute Projects/Frontend/src/App.jsx
+++ BOI Training Institute Projects/Frontend/src/App.jsx
@@ -1,5 +1,6 @@
  import React from "react"
 import LocationForm from "./components/LocationForm"
+import EquipmentTable from "./components/EquipmentTable"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <LocationForm/>
+      <EquipmentTable/>
     </>
   )
 }
