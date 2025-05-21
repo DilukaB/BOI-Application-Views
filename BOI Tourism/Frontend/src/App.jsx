4c01319a44252442f5c2c02db5cdb6c8ecc2f797
DilukaB <navanjana721@gmail.com>
2025-05-21 09:52:14 +0530

    h

--- BOI Tourism/Frontend/src/App.jsx
+++ BOI Tourism/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
- import React, { useRef } from "react";
+  import React, { useRef } from "react";
 import jsPDF from "jspdf";
 import domtoimage from "dom-to-image";
 import TourismProject from "./components/TourismProject";
